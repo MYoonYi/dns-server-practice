eb56a08cd81f188ae150d807fceea13a34a94afb
MYoonYi <myoonyi@aol.com>
2025-04-14 20:25:47 +0200

    ✨ new(main): create a DNS server

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,21 @@
+use std::net::UdpSocket;
+
+use dns::Header;
+
 mod dns;
 
 fn main() {
-    println!("Hello, world!");
+    let socket = UdpSocket::bind("0.0.0.0:1053").expect("Could not bind to port 1053");
+    let mut buf = [0; 512];
+
+    println!("DNS Server is running at port 1053");
+
+    loop {
+        let (len, addr) = socket
+            .recv_from(&mut buf)
+            .expect("Couldn't bind to the adress.");
+        let header = Header::from_bytes(&buf[..len]).expect("Could not parse DNS Header.");
+
+        println!("Received query from {} {:?}", addr, header);
+    }
 }
